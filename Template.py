e0cb727f5f2254cb26023f7767c1b02d267352b4
Chainfury <82396613+Chainfury@users.noreply.github.com>
2022-03-06 17:17:03 +0000

    Modified Version of Zom's template, easier for Chainfury to understand

---
+++ Template.py
@@ -0,0 +1,39 @@
+from typing import List
+
+def readFile(file: str) -> List[str]:
+    """Reads file - takes string in and claims to return list of string
+
+    Args:
+        file (str): The name of the file
+
+    Returns:
+        List[str]: List of the lines in the file
+    """
+    with open(file, 'r') as file: #uses with as removes need for manual file close
+        lines = file.readlines()
+        
+    return [line.strip() for line in lines]
+
+def writeFile(name: str, data: List):
+    """Writes to given file
+
+    Args:
+        path (str): The file name
+        data (List): The data to be put on the file as a list
+    """
+
+    with open(name, 'w') as file:
+        file.write("\n".join([str(record) for record in data])) #adds new line to each data row to display an so of lines
+
+def main():
+    data = readfile("{data.txt}") #Replace with file location
+    output = []
+    
+    for i in range(0, int(data.pop(0))):
+        #Code here
+        pass
+    
+    writeFile("{result.txt}", output)
+
+if __name__ == "__main__":
+    main()
